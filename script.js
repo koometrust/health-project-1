aeebc93bf969e815e4a940a27bdfcc74e83d41b1
Koome <judekoome360@gmail.com>
2023-04-07 22:56:58 +0300

    moved the BMI result higher

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const heightInput = document.getElementById('height');
   const weightInput = document.getElementById('weight');
   const bmiResult = document.getElementById('bmi-result');
-
+  const healthStatus = document.getElementById('health-status'); // added new element
+  
   form.addEventListener('submit', (e) => {
     e.preventDefault(); // prevent form submission
     const height = heightInput.value;
@@ -11,6 +12,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const bmi = calculateBMI(height, weight);
     bmiResult.innerText = `Your BMI is ${bmi.toFixed(2)}.`;
 
+    // Determine health status based on BMI value
+    let status;
+    if (bmi < 18.5) {
+      status = 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 25) {
+      status = 'Normal weight';
+    } else if (bmi >= 25 && bmi < 30) {
+      status = 'Overweight';
+    } else {
+      status = 'Obese';
+    }
+    healthStatus.innerText = `Based on your BMI, your health status is ${status}.`; // added new line
+    
     // Determine appropriate nutrient content based on BMI category
     let nutrient;
     if (bmi < 18.5) {
